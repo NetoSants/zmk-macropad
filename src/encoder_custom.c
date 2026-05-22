3e5b856d1302607c0d0a6072900fe41ab3388df8
Neto Santos <fernandobezerrados@gmail.com>
2026-05-22 01:51:30 -0300

    test: minimal driver, ISR only, no LED no HID

--- src/encoder_custom.c
+++ src/encoder_custom.c
@@ -1,66 +1,13 @@
 #include <zephyr/kernel.h>
 #include <zephyr/device.h>
 #include <zephyr/drivers/gpio.h>
-#include <zmk/hid.h>
-#include <zmk/endpoints.h>
-#include <dt-bindings/zmk/hid_usage.h>
-#include <dt-bindings/zmk/hid_usage_pages.h>
 #include "encoder_custom.h"
 
 static const struct device *gpio_dev;
 static struct gpio_callback enc_cb;
-static struct k_work enc_work;
-static struct k_work_delayable enc_release_work;
-
-static volatile int8_t position;
-static volatile uint8_t last_state;
-static volatile int8_t last_dir;
-
-static const int8_t enc_steps[] = {
-    0, 1, -1, 0, -1, 0, 0, 1, 1, 0, 0, -1, 0, -1, 1, 0,
-};
 
 static void enc_isr(const struct device *dev, struct gpio_callback *cb, uint32_t pins)
 {
-    uint8_t a = gpio_pin_get(gpio_dev, ENC_A_PIN);
-    uint8_t b = gpio_pin_get(gpio_dev, ENC_B_PIN);
-    uint8_t state = (a << 1) | b;
-    uint8_t idx = (last_state << 2) | state;
-    last_state = state;
-
-    int8_t step = enc_steps[idx & 0x0F];
-    if (step) {
-        position += step;
-        last_dir = step;
-        k_work_submit(&enc_work);
-    }
-}
-
-static void enc_release_handler(struct k_work_delayable *work)
-{
-    uint32_t usage = last_dir > 0
-        ? (HID_USAGE_CONSUMER << 16) | HID_USAGE_CONSUMER_VOLUME_INCREMENT
-        : (HID_USAGE_CONSUMER << 16) | HID_USAGE_CONSUMER_VOLUME_DECREMENT;
-    zmk_hid_release(usage);
-    zmk_endpoints_send_report();
-}
-
-static void enc_work_handler(struct k_work *work)
-{
-    int8_t pos = position;
-    position = 0;
-
-    if (pos > 0) {
-        uint32_t usage = (HID_USAGE_CONSUMER << 16) | HID_USAGE_CONSUMER_VOLUME_INCREMENT;
-        zmk_hid_press(usage);
-        zmk_endpoints_send_report();
-        k_work_reschedule(&enc_release_work, K_MSEC(RELEASE_MS));
-    } else if (pos < 0) {
-        uint32_t usage = (HID_USAGE_CONSUMER << 16) | HID_USAGE_CONSUMER_VOLUME_DECREMENT;
-        zmk_hid_press(usage);
-        zmk_endpoints_send_report();
-        k_work_reschedule(&enc_release_work, K_MSEC(RELEASE_MS));
-    }
 }
 
 static int encoder_init(void)
@@ -72,11 +19,6 @@ static int encoder_init(void)
 
     gpio_pin_configure(gpio_dev, ENC_A_PIN, GPIO_INPUT | GPIO_PULL_UP);
     gpio_pin_configure(gpio_dev, ENC_B_PIN, GPIO_INPUT | GPIO_PULL_UP);
-
-    last_state = (gpio_pin_get(gpio_dev, ENC_A_PIN) << 1) | gpio_pin_get(gpio_dev, ENC_B_PIN);
-
-    k_work_init(&enc_work, enc_work_handler);
-    k_work_init_delayable(&enc_release_work, enc_release_handler);
 
     gpio_init_callback(&enc_cb, enc_isr, BIT(ENC_A_PIN) | BIT(ENC_B_PIN));
     gpio_add_callback(gpio_dev, &enc_cb);
--- src/encoder_custom.h
+++ src/encoder_custom.h
@@ -2,7 +2,3 @@
 
 #define ENC_A_PIN 13
 #define ENC_B_PIN 15
-
-#define STEPS_PER_DETENT 4
-#define DEBOUNCE_US 200
-#define RELEASE_MS 15
